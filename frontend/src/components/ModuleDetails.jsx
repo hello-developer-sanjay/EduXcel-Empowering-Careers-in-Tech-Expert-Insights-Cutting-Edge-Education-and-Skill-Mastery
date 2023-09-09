8cc9adb76033c43c721bf0af34d748ebba91498e
hello-developer-sanjay <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-10 00:44:05 +0530

    Update ModuleDetails.jsx

--- frontend/src/components/ModuleDetails.jsx
+++ frontend/src/components/ModuleDetails.jsx
@@ -14,7 +14,7 @@ function ModuleDetails() {
     async function fetchModuleDetails() {
       try {
         const response = await axios.get(
-          `http://localhost:5000/api/courses/${encodeURIComponent(title)}/modules/${encodeURIComponent(module)}`
+          `https://xcel-back.onrender.com/api/courses/${encodeURIComponent(title)}/modules/${encodeURIComponent(module)}`
         );
 
        
