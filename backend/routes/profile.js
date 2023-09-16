266d05200c4a681e21b2b463a18380e30fd95f61
hello-developer-sanjay <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-16 18:02:36 +0530

    Update profile.js

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,4 +1,4 @@
-"const path = require('path');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
