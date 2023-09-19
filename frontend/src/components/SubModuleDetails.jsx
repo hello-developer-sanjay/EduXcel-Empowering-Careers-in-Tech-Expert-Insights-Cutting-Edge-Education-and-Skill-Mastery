639c057f2ac6f355efcf24b377ccb9fe279d19b6
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-19 19:05:48 +0530

    Update SubModuleDetails.jsx

--- frontend/src/components/SubModuleDetails.jsx
+++ frontend/src/components/SubModuleDetails.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import ReactPlayer from 'react-player';
-import '../styles/ModuleDetails.css'; // Import the CSS file for styling
+import '../styles/SubModuleDetails.css'; // Import the CSS file for styling
 
 function SubModuleDetails() {
   const { title, module, submodule } = useParams();
@@ -17,7 +16,6 @@ function SubModuleDetails() {
           `https://xcel-back.onrender.com/api/courses/${encodeURIComponent(title)}/modules/${encodeURIComponent(module)}/${encodeURIComponent(submodule)}`
         );
 
-        
         setSubModuleDetails(response.data);
         setLoading(false);
       } catch (error) {
@@ -31,30 +29,76 @@ function SubModuleDetails() {
   }, [title, module, submodule]);
 
   if (loading) {
-    return <div className="module-container loading">Loading...</div>;
+    return <div className="sub-module-container loading">Loading...</div>;
   }
 
   if (error) {
-    return <div className="module-container error">Error: {error}</div>;
+    return <div className="sub-module-container error">Error: {error}</div>;
   }
 
   if (!subModuleDetails) {
-    return <div className="module-container not-found">Sub-module not found.</div>;
+    return <div className="sub-module-container not-found">Sub-module not found.</div>;
   }
 
-  return (
-    <div className="module-container">
-      <div className="module-details">
-        <h2>Sub-Module Details</h2>
-        <p>Title: {subModuleDetails.title}</p>
-        <p>Description: {subModuleDetails.description}</p>
-        <div className="video-container">
-          <div className="video-wrapper">
-            <ReactPlayer url={subModuleDetails.videoURL} controls className="react-player" />
-          </div>
+  const renderDescriptionItem = (item, index) => {
+    if (Array.isArray(item)) {
+      return (
+        <div key={index}>
+          {item.map((subItem, subIndex) => {
+            if (subItem.startsWith('http')) {
+              if (subItem.endsWith('.mp4')) {
+                return (
+                  <div key={subIndex} className="sub-video-container">
+                    <video controls className="sub-module-media">
+                      <source src={subItem} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  </div>
+                );
+              } else if (subItem.endsWith('.jpg')) {
+                return (
+                  <div key={subIndex} className="sub-image-container">
+                    <img src={subItem} alt="Image" className="sub-module-image" />
+                  </div>
+                );
+              }
+            }
+
+            return <p key={subIndex}>{subItem}</p>;
+          })}
         </div>
-        <p>Content: {subModuleDetails.content}</p>
-        <img src={subModuleDetails.imageURL} alt={subModuleDetails.title} className="module-image" />
+      );
+    }
+
+    return <p key={index}>{item}</p>;
+  };
+
+  return (
+    <div className="sub-module-container">
+      <div className="sub-module-details">
+        <h2 className="sub-module-title">Sub-Module: {subModuleDetails.title}</h2>
+        <p className="sub-module-description">{subModuleDetails.content.introduction}</p>
+
+        <div className="sub-module-content">
+          {subModuleDetails.content.sections.map((section, index) => (
+            <div className="sub-module-section" key={index}>
+              <h3 className="sub-section-title">{section.title}</h3>
+              <div className="sub-section-description">
+                {renderDescriptionItem(section.description, index)}
+              </div>
+              <ul className="sub-element-list">
+                {section.elements.map((element, idx) => (
+                  <li className="sub-element-item" key={idx}>
+                    <span className="sub-element-tag">{element.tag}</span>
+                    <span className="sub-element-description">
+                      {renderDescriptionItem(element.description, idx)}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
