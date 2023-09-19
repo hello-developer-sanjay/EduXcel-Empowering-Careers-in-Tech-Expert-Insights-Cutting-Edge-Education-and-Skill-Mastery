5496b5ea4847e00ed263d8c89c751730ade6e0d7
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-19 19:05:26 +0530

    Update ModuleDetails.jsx

--- frontend/src/components/ModuleDetails.jsx
+++ frontend/src/components/ModuleDetails.jsx
@@ -17,7 +17,6 @@ function ModuleDetails() {
           `https://xcel-back.onrender.com/api/courses/${encodeURIComponent(title)}/modules/${encodeURIComponent(module)}`
         );
 
-       
         setModuleDetails(response.data);
         setLoading(false);
       } catch (error) {
@@ -55,10 +54,100 @@ function ModuleDetails() {
                   {item.title}
                 </Link>
               </h3>
-              <p>Description: {item.description}</p>
+              <p className="module-description">Description: {item.details}</p>  {/* Display additional details */}
+              {item.elements && (
+                <div>
+                  <h4>HTML Elements:</h4>
+                  <ul>
+                    {item.elements.map((element, elementIndex) => (
+                      <li key={elementIndex}>{element}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.styling_options && (
+                <div>
+                  <h4>CSS Styling Options:</h4>
+                  <ul>
+                    {item.styling_options.map((option, optionIndex) => (
+                      <li key={optionIndex}>{option}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.javascript_features && (
+                <div>
+                  <h4>JavaScript Features:</h4>
+                  <ul>
+                    {item.javascript_features.map((feature, featureIndex) => (
+                      <li key={featureIndex}>{feature}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.responsive_techniques && (
+                <div>
+                  <h4>Responsive Techniques:</h4>
+                  <ul>
+                    {item.responsive_techniques.map((technique, techniqueIndex) => (
+                      <li key={techniqueIndex}>{technique}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.frontend_frameworks && (
+                <div>
+                  <h4>Frontend Frameworks:</h4>
+                  <ul>
+                    {item.frontend_frameworks.map((framework, frameworkIndex) => (
+                      <li key={frameworkIndex}>{framework}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.topics && (
+                <div>
+                  <h4>Topics:</h4>
+                  <ul>
+                    {item.topics.map((topic, topicIndex) => (
+                      <li key={topicIndex}>{topic}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.ml_types && (
+                <div>
+                  <h4>Machine Learning Types:</h4>
+                  <ul>
+                    {item.ml_types.map((mlType, mlTypeIndex) => (
+                      <li key={mlTypeIndex}>{mlType}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.ml_algorithms && (
+                <div>
+                  <h4>Machine Learning Algorithms:</h4>
+                  <ul>
+                    {item.ml_algorithms.map((mlAlgorithm, mlAlgorithmIndex) => (
+                      <li key={mlAlgorithmIndex}>{mlAlgorithm}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {item.feature_engineering && (
+                <div>
+                  <h4>Feature Engineering:</h4>
+                  <ul>
+                    {item.feature_engineering.map((featureEng, featureEngIndex) => (
+                      <li key={featureEngIndex}>{featureEng}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
               <div className="video-container">
-                <div className="video-wrapper"> {/* Add this div wrapper */}
-                  <ReactPlayer url={item.videoURL} controls className="react-player" /> {/* Add 'react-player' class */}
+                <div className="video-wrapper">
+                  <ReactPlayer url={item.videoURL} controls className="react-player" />
                 </div>
               </div>
               <img src={item.imageURL} alt={item.title} className="module-image" />
@@ -68,10 +157,6 @@ function ModuleDetails() {
       </div>
     </div>
   );
- 
-  
-  
-  
 }
 
 export default ModuleDetails;
