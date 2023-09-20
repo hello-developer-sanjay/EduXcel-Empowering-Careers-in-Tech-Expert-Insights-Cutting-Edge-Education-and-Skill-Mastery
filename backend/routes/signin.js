ab2d8e6d29feb50c2243f9f61faaa015895578f4
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-20 22:57:57 +0530

    Update signin.js

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer'); // Import nodemailer for sending email
 require('dotenv').config();
 
 // Function to send a welcome email
-const sendWelcomeEmail = async (email) => {
+const sendWelcomeEmail = async (email, userName) => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
@@ -19,104 +19,105 @@ const sendWelcomeEmail = async (email) => {
   const mailOptions = {
     from: process.env.EMAIL_ADDRESS,
     to: email,
-    subject: 'Welcome back to Eduxcel!',
- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Eduxcel</title>
-    <style>
-        /* General styles for the email */
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-        }
+    subject: 'Embrace Eduxcel: Welcome to the Community!',
+    html: `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Welcome to YourApp</title>
+          <style>
+              /* General styles for the email */
+              body {
+                  font-family: Arial, sans-serif;
+                  background-color: #f4f4f4;
+                  margin: 0;
+                  padding: 0;
+              }
 
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #ffffff;
-            border-radius: 10px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-        }
+              .container {
+                  max-width: 600px;
+                  margin: 0 auto;
+                  padding: 20px;
+                  background-color: #ffffff;
+                  border-radius: 10px;
+                  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+              }
 
-        h1 {
-            font-size: 32px;
-            color: #333;
-            margin-bottom: 20px;
-        }
+              h1 {
+                  font-size: 32px;
+                  color: #333;
+                  margin-bottom: 20px;
+              }
 
-        p {
-            font-size: 18px;
-            color: #555;
-            margin-bottom: 16px;
-        }
+              p {
+                  font-size: 18px;
+                  color: #555;
+                  margin-bottom: 16px;
+              }
 
-        ul {
-            margin: 16px 0;
-            padding-left: 20px;
-        }
+              ul {
+                  margin: 16px 0;
+                  padding-left: 20px;
+              }
 
-        li {
-            font-size: 18px;
-            color: #555;
-            margin-bottom: 10px;
-        }
+              li {
+                  font-size: 18px;
+                  color: #555;
+                  margin-bottom: 10px;
+              }
 
-        .button {
-            display: inline-block;
-            padding: 12px 24px;
-            background-color: #007BFF;
-            color: #ffffff;
-            text-decoration: none;
-            border-radius: 5px;
-        }
+              .button {
+                  display: inline-block;
+                  padding: 12px 24px;
+                  background-color: #007BFF;
+                  color: #ffffff;
+                  text-decoration: none;
+                  border-radius: 5px;
+              }
 
-        .button:hover {
-            background-color: #0056b3;
-        }
+              .button:hover {
+                  background-color: #0056b3;
+              }
 
-        .signature {
-            font-size: 24px;
-            font-weight: bold;
-            margin-top: 20px;
-        }
+              .signature {
+                  font-size: 24px;
+                  font-weight: bold;
+                  margin-top: 20px;
+              }
 
-        /* Style for the header image */
-        .header-image {
-            width: 100%;
-            max-height: 200px;
-            object-fit: cover;
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-         <img class="header-image" src="https://sanjaybasket.s3.ap-south-1.amazonaws.com/logo.png" alt="Welcome to Eduxcel">
-        <h1>Welcome to Eduxcel</h1>
-        <h2>Dear ${username},</h2>
-        <p>Welcome back to <strong>YourApp</strong>! We're thrilled to have you on board.</p>
-        <p>Here's what you can do now:</p>
-        <ul>
-            <li>Explore our latest features and updates.</li>
-            <li>Connect with other users and start engaging with our community.</li>
-            <li>Discover exciting content tailored just for you.</li>
-            <li>Stay updated with personalized notifications.</li>
-        </ul>
-        <p>If you have any questions, need assistance, or want to share feedback, don't hesitate to reach out to us.</p>
-        <p>Thank you for choosing <strong>Eduxcel</strong> for your online journey. We're here to make it awesome!</p>
-        <a href="https://eduxcel.vercel.app" class="button">Start Exploring</a>
-        <p class="signature">Best regards,</p>
-        <p>Eduxcel Team</p>
-    </div>
-</body>
-</html>
+              /* Style for the header image */
+              .header-image {
+                  width: 100%;
+                  max-height: 200px;
+                  object-fit: cover;
+                  border-top-left-radius: 10px;
+                  border-top-right-radius: 10px;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <img class="header-image" src="https://sanjaybasket.s3.ap-south-1.amazonaws.com/logo.png" alt="Welcome to Eduxcel">
+              <h1>Welcome to Eduxcel</h1>
+              <p>Dear ${userName},</p>
+              <p>Welcome back to <strong>Eduxcel</strong>! We're thrilled to have you on board.</p>
+              <p>Here's what you can do now:</p>
+              <ul>
+                  <li>Explore our latest features and updates.</li>
+                  <li>Connect with other users and start engaging with our community.</li>
+                  <li>Discover exciting content tailored just for you.</li>
+                  <li>Stay updated with personalized notifications.</li>
+              </ul>
+              <p>If you have any questions, need assistance, or want to share feedback, don't hesitate to reach out to us.</p>
+              <p>Thank you for choosing <strong>YourApp</strong> for your online journey. We're here to make it awesome!</p>
+              <a href="https://eduxcel.vercel.app" class="button">Start Exploring</a>
+              <p class="signature">Best regards,</p>
+              <p>Eduxcel Team</p>
+          </div>
+      </body>
+      </html>
     `,
   };
 
@@ -141,7 +142,7 @@ router.post('/', async (req, res) => {
     const token = jwt.sign({ userId: user._id }, 'fRwD8ZcX#k5H*J!yN&2G@pQbS9v6E$tA');
 
     // Send a welcome email to the user
-    await sendWelcomeEmail(email);
+    await sendWelcomeEmail(email, user.username);
 
     res.status(200).json({ token });
   } catch (error) {
