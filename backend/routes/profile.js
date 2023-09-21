77038d75901e5bb52bea3ca67b67a3bd69b3e0fe
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-21 19:11:20 +0530

    Update profile.js

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,49 +1,47 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const UserProfile = require('../models/UserProfile');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
-const AWS = require('aws-sdk');
-const multerS3 = require('multer-s3');
 
-const s3 = new AWS.S3({
-  accessKeyId: '0EgD/s8cvM0UYF/KghYfxgYGlJzNcM3ecuRfGUyJ',
-  secretAccessKey: 'AKIA5BQ4NJCXMEDIBQUS',
-  region: 'ap-south-1',
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Specify the directory where uploaded images will be stored
+  },
+  filename: (req, file, cb) => {
+    const uniqueFilename = `${uuidv4()}${path.extname(file.originalname)}`;
+    cb(null, uniqueFilename); // Set the filename to be unique
+  },
 });
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'profileusersupload',
-    acl: 'public-read',
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      cb(null, 'profileImages/' + uuidv4() + '-' + file.originalname);
-    },
-  }),
+const upload = multer({ 
+  storage: storage,
   fileFilter: (req, file, callback) => {
-    const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
+    const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif']; // Add allowed extensions
     const ext = path.extname(file.originalname);
     if (!allowedExtensions.includes(ext)) {
       return callback(new Error('Only image files are allowed.'));
     }
     callback(null, true);
-  },
+  }
 });
 
 // Fetch user profile route
 router.get('/', authMiddleware, async (req, res) => {
   try {
+    console.log('Received a request to fetch user profile');
+    console.log('User ID:', req.user._id);
+
     const userProfile = await UserProfile.findOne({ user: req.user._id });
 
     if (!userProfile) {
+      console.log('User profile not found');
       return res.status(404).json({ message: 'User profile not found' });
     }
 
+    console.log('Fetched user profile:', userProfile);
     res.status(200).json(userProfile);
   } catch (error) {
     console.error('Error fetching user profile:', error);
@@ -54,21 +52,27 @@ router.get('/', authMiddleware, async (req, res) => {
 // Update user profile route with file upload
 router.put('/', authMiddleware, upload.single('profileImage'), async (req, res) => {
   try {
+    console.log('Received a request to update user profile');
+    console.log('User ID:', req.user._id);
+    console.log('Request Body:', req.body);
+
     const userProfile = await UserProfile.findOneAndUpdate(
       { user: req.user._id },
       {
         firstName: req.body.firstName || '',
         lastName: req.body.lastName || '',
         bio: req.body.bio || '',
-        profileImage: req.file ? req.file.location : '', // Use the S3 object URL
+        profileImage: req.file ? req.file.filename : '', // Use the filename provided by multer
       },
-      { new: true }
+      { new: true } // Use the { new: true } option to get the updated document
     );
 
     if (!userProfile) {
+      console.log('User profile not found');
       return res.status(404).json({ message: 'User profile not found' });
     }
 
+    console.log('Updated user profile:', userProfile);
     res.status(200).json(userProfile);
   } catch (error) {
     console.error('Error updating user profile:', error);
