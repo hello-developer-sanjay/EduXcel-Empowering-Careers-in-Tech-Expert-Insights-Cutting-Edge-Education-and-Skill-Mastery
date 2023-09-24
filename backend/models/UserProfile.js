c3039cdf7200fa7db0e1a9e7e8d4c24d31a1e21a
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-25 00:16:42 +0530

    Update UserProfile.js

--- backend/models/UserProfile.js
+++ backend/models/UserProfile.js
@@ -1,14 +1,24 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const userProfileSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  username: String,
-  email: String,
+const userProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User model
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
   firstName: String,
   lastName: String,
   bio: String,
-  profileImage: String, // You can store the image URL here
-  // Add more profile fields as needed
+  profileImage: String,
 });
 
 module.exports = mongoose.model('UserProfile', userProfileSchema);
