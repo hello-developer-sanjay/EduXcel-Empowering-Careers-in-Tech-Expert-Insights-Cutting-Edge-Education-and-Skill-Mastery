c8110bb0d0242017946b1452aca59f3b1b30ff53
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-25 00:55:58 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+   password: { type: String },
   resetToken: String, // Add resetToken field
   resetTokenExpiration: Date, // Add resetTokenExpiration field
 });
