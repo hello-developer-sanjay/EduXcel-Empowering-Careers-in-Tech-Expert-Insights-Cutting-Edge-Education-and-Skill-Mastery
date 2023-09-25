2dfc7ca418830c66611e9ed0bcf45ef6daf5f13e
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-25 13:27:54 +0530

    Update UserProfile.js

--- backend/models/UserProfile.js
+++ backend/models/UserProfile.js
@@ -1,20 +1,9 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userProfileSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User', // Reference to the User model
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
+const userProfileSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Assuming a reference to the User model
+  email: { type: String, required: true }, // Make sure these fields are marked as required if needed
+  username: { type: String, required: true },
   firstName: String,
   lastName: String,
   bio: String,
