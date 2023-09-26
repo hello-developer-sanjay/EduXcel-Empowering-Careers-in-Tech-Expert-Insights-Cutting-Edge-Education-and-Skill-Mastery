1ea00d75132521131ce769309ee65a23dd491a2a
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-26 21:28:46 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-const User = require('../models/User'); // Import your User model
+const User = require('../models/User'); 
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = async (req, res, next) => {
