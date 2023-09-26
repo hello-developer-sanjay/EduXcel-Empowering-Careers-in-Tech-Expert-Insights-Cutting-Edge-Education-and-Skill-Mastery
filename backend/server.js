cae29d482012e8ac03cfeae7a4e043ae37e1c565
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-26 15:30:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const router = express.Router();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
