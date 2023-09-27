df168dd94f69ed4ad826b104e492dc4e61fcb10f
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-09-27 23:34:20 +0530

    Update workbox-config.js

--- frontend/workbox-config.js
+++ frontend/workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     globDirectory: 'dist', // Your build output directory
     globPatterns: ['**/*.{html,js,css,png,jpg,svg,json}'],
-    swDest: 'dist/sw.js', // Output service worker file
+    swDest: 'dist/sw-v2.js', // Output service worker file
     clientsClaim: true,
     skipWaiting: true,
   };
