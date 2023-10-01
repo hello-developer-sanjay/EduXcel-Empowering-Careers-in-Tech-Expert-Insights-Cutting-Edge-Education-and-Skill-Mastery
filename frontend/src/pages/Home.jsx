821a084d18544b06ce60259634d4b03ce6a4487b
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-01 14:51:43 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,14 @@ import axios from 'axios'; // Import axios for making API requests
 
 function Home() {
   const [courseData, setCourseData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    
     async function fetchCourses() {
       try {
-       const response = await axios.get('https://xcel-back.onrender.com/api/courses');
+        const response = await axios.get('https://xcel-back.onrender.com/api/courses');
         setCourseData(response.data); // Assuming your API response is an array of course objects
       } catch (error) {
         console.error('Error fetching courses:', error);
@@ -35,6 +37,28 @@ function Home() {
     });
   }
 
+  useEffect(() => {
+    async function searchCourses() {
+      if (searchTerm.trim() === '') {
+        setSearchResults([]);
+        return;
+      }
+
+      setLoading(true);
+
+      try {
+        const response = await axios.get(`https://xcel-back.onrender.com/api/search?query=${encodeURIComponent(searchTerm)}`);
+        setSearchResults(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error searching for courses:', error);
+        setLoading(false);
+      }
+    }
+
+    searchCourses();
+  }, [searchTerm]);
+
   return (
     <section className={`relative w-full min-h-screen mx-auto overflow-y-auto`}>
       <Helmet>
@@ -55,6 +79,37 @@ function Home() {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Explore our courses and enhance your skills
           </p>
+
+          {/* Add a search bar */}
+          <div className='mt-6'>
+            <input
+              type='text'
+              placeholder='Search for courses...'
+              className='border border-gray-300 rounded-lg p-2 w-full'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+
+          {/* Display search results */}
+          {loading && <p>Loading...</p>}
+          {!loading && searchResults.length === 0 && searchTerm.trim() !== '' && (
+            <p>No results found for "{searchTerm}"</p>
+          )}
+          {!loading && searchResults.length > 0 && (
+            <div className='mt-4'>
+              <h2 className='text-lg font-semibold'>Search Results</h2>
+              <ul>
+                {searchResults.map((result) => (
+                  <li key={result._id}>
+                    <a href={`/courses/${result.title}`}>{result.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {/* Display the CourseList */}
           <div className='mt-10'>
             <CourseList courseData={courseData} /> {/* Pass course data as a prop to CourseList component */}
           </div>
