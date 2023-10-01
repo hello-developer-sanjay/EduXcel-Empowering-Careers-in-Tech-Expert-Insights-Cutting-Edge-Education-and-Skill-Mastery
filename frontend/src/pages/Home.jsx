59c40a44b5ddc941a465139d568e10fbc83e8267
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-01 21:38:28 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import CourseList from '../components/CourseList';
 import { Helmet } from 'react-helmet';
-import { styles } from '../styles';
 import axios from 'axios'; // Import axios for making API requests
 
 function Home() {
@@ -38,8 +37,8 @@ function Home() {
   return (
     <section className={`relative w-full min-h-screen mx-auto overflow-y-auto`}>
       <Helmet>
-        <title>Eduxcel - Online Education Platform</title>
-        <meta name="description" content="Explore our courses and enhance your skills with Eduxcel. Find a wide range of online courses on various topics to boost your knowledge." />
+        <title>EduXcel E-Learning Platform</title>
+        <meta name="description" content="EduXcel is a comprehensive e-learning platform that offers high-quality courses and personalized learning experiences. Explore our courses and enhance your skills with EduXcel." />
         {/* Add structured data for CourseList */}
         <script type="application/ld+json">
           {JSON.stringify({
@@ -52,12 +51,28 @@ function Home() {
 
       <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Explore our courses and enhance your skills
-          </p>
-          <div className='mt-10'>
-            <CourseList courseData={courseData} /> {/* Pass course data as a prop to CourseList component */}
-          </div>
+          <h1>Welcome to EduXcel E-Learning Platform</h1>
+          <p>We are thrilled to introduce you to our innovative project, designed to revolutionize online education.</p>
+
+          <section>
+            <h2>Purpose</h2>
+            <p>EduXcel is a comprehensive e-learning platform aimed at enhancing the learning experience for both students and instructors. Our primary goals are:</p>
+            <ul>
+              <li>To provide a diverse range of high-quality courses and educational content.</li>
+              <li>To create a seamless and secure environment for online education.</li>
+              <li>To offer personalized learning experiences tailored to individual needs.</li>
+            </ul>
+          </section>
+
+          <section>
+            <h2>User Experience</h2>
+            <p>At EduXcel, we prioritize user satisfaction. Here's what you can expect from our platform:</p>
+            <ul>
+              <li><strong>Secure Sign-Up:</strong> User data is protected with state-of-the-art security measures.</li>
+              <li><strong>Personalized Profiles:</strong> Users can create and customize profiles to track their progress.</li>
+              <li><strong>Tailored Recommendations:</strong> Our recommendation system suggests courses based on user preferences and learning history.</li>
+            </ul>
+          </section>
         </div>
       </div>
     </section>
