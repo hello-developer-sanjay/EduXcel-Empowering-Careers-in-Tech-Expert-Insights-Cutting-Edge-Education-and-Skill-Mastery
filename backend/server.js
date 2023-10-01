32580f8a34eb14aeb431a350cc1a1b05b5a85942
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-01 16:00:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -135,35 +135,6 @@ app.get('/uploads/:filename', (req, res) => {
   res.setHeader('Cache-Control', 'no-store'); // Disable caching
   res.sendFile(path.join(__dirname, 'uploads', req.params.filename));
 });
-// Search for courses and modules based on a query parameter
-app.get('/search', async (req, res) => {
-  try {
-    const query = req.query.query;
-
-    if (!query) {
-      return res.status(400).json({ error: 'Query parameter is missing' });
-    }
-
-    // Use a case-insensitive regular expression for searching
-    const regex = new RegExp(query, 'i');
-
-    // Search for courses by title or description
-    const courses = await Course.find({
-      $or: [{ title: regex }, { description: regex }],
-    });
-
-    // Search for modules by title or description
-    const modules = await Module.find({
-      $or: [{ title: regex }, { description: regex }],
-    });
-
-    // Combine and send both courses and modules in the response
-    res.json({ courses, modules });
-  } catch (error) {
-    console.error('Error searching for courses and modules:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
 
 
