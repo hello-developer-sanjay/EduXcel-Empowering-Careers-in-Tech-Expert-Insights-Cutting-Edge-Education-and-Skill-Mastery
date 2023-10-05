a8e260c309bba258e75486e1943558e78c9dd1e7
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-05 17:49:41 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/background.jpg')",
+        "hero-pattern": "url('/src/assets/background.webp')",
       },
     },
   },
