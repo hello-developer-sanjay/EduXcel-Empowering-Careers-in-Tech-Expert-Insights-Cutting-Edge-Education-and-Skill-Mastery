6fbc4466fef49210d8033b1d00b427ebde22bee9
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-10-13 19:15:00 +0530

    Update SubHeader.jsx

--- frontend/src/components/SubHeader.jsx
+++ frontend/src/components/SubHeader.jsx
@@ -11,7 +11,7 @@ function SubHeader() {
   useEffect(() => {
     async function fetchCourses() {
       try {
-        const response = await axios.get('https://xcel-back.onrender.com/api/courses');
+        const response = await axios.get('https://backend-uxyx.onrender.com/api/courses');
         setCourses(response.data);
       } catch (error) {
         console.error('Error fetching courses:', error);
