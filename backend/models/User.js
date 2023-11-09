6f2005ded8186321ee894b48b3e75a586f708a1d
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-11-09 10:08:39 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   googleId: String, // Store Google ID for Google-authenticated users
   resetToken: String,
+  password: { type: String, required: true },
   resetTokenExpiration: Date,
 });
 
