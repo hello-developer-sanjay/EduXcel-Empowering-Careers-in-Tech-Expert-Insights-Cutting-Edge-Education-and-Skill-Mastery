1dbb4f488565aed5d71dc88b1e5c8a6071ba969e
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-11-10 12:21:14 +0530

    Update UserProfile.jsx

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import EditProfile from './EditProfile';
 import '../styles/UserProfile.css';
