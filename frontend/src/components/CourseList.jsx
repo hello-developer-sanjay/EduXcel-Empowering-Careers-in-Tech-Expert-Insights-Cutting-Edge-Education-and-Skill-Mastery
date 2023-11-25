a68b9996b87bd66c9f2596dc800cee2126591b94
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-11-25 12:19:15 +0530

    Update CourseList.jsx

--- frontend/src/components/CourseList.jsx
+++ frontend/src/components/CourseList.jsx
@@ -14,7 +14,7 @@ function CourseList() {
   useEffect(() => {
     async function fetchCourses() {
       try {
-        const response = await axios.get('https://eduxcel-backend.onrender.com/api/courses');
+        const response = await axios.get('https://edu-back-j3mz.onrender.com/api/courses');
         setCourses(response.data);
       } catch (error) {
         console.error('Error fetching courses:', error);
