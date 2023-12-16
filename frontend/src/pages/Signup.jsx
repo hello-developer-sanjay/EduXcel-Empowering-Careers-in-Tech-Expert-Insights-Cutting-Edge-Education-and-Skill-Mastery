c15ce21b0f6386d3194fe7203cc363706acb35f4
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-12-16 09:55:44 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,6 +4,7 @@ import '../styles/AuthForms.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import validator from 'validator';
+
 function Signup() {
   const [formData, setFormData] = useState({
     username: '',
@@ -27,22 +28,25 @@ function Signup() {
     setShowPassword(!showPassword);
   };
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    if (!validator.isEmail(formData.email)) {
-      setSignupError('Invalid email format');
-      return;
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (!validator.isEmail(formData.email)) {
+        setSignupError('Invalid email format');
+        return;
+      }
 
-    const response = await axios.post('https://edu-back-j3mz.onrender.com/api/signup', formData);
-    console.log('Signup success');
-    setSignupSuccess('Signup successful!');
-  } catch (error) {
-    console.error('Signup error:', error.response.data.message);
-    setSignupError(error.response.message);
-  }
-};
+      const response = await axios.post(
+        'https://edu-back-j3mz.onrender.com/api/signup',
+        formData
+      );
+      console.log('Signup success');
+      setSignupSuccess('Signup successful!');
+    } catch (error) {
+      console.error('Signup error:', error.response.data.message);
+      setSignupError(error.response.message);
+    }
+  };
 
   return (
     <div className="form-container">
@@ -74,7 +78,10 @@ const handleSubmit = async (e) => {
           Signup
         </button>
         {signupError && <p className="error-message">{signupError}</p>}
-        {signupSuccess && <p className="success-message">{signupSuccess}</p>} {/* Render signup success message */}
+        {signupSuccess && (
+          <p className="success-message">{signupSuccess}</p>
+        )}{' '}
+        {/* Render signup success message */}
       </form>
     </div>
   );
