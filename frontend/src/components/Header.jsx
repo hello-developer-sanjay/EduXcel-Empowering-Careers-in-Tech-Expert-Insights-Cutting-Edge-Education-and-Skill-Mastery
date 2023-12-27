103918291cebf0e05a78b214b1345576a8d3567f
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-12-27 22:14:29 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,31 +1,27 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import Typed from 'react-typed';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUserPlus, faNewspaper } from '@fortawesome/free-solid-svg-icons'; // Import FontAwesome icons
+import { faHome, faUserPlus, faNewspaper } from '@fortawesome/free-solid-svg-icons';
 
 const HeaderContainer = styled.header`
-  background: linear-gradient(to right, #3498db, #2c3e50); /* Gradient background */
+background: linear-gradient(to right, #1a1a2e, #16213e, #0d1b2a);
   color: #fff;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  
-  padding: 0rem;
-    flex-wrap: wrap;
+  padding: 0.5rem;
   position: sticky;
   top: 0;
   z-index: 1000;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); 
-  border-bottom: 2px solid #2980b9; 
-
-
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  border-bottom: 2px solid #2980b9;
   transition: background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
 
-    background: linear-gradient(to right, #2c3e50, #3498db); /* Gradient background on hover */
-    
-  
+  @media (max-width: 768px) {
+    padding: 1rem 0.5rem;
+  }
 `;
 
 const HeaderContent = styled.div`
@@ -35,7 +31,7 @@ const HeaderContent = styled.div`
 `;
 
 const Logo = styled.h1`
-  font-size: 24px;
+  font-size: 0px;
   margin: 0;
   color: #fff;
 `;
@@ -66,8 +62,6 @@ const EduxcelText = styled.span`
   }
 `;
 
-
-
 const NavList = styled.ul`
   list-style-type: none;
   margin: 0;
@@ -76,13 +70,15 @@ const NavList = styled.ul`
   gap: 20px;
   flex-wrap: wrap;
 
-  
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 10px;
+    align-items: flex-start;
+  }
 `;
 
 const NavItem = styled.li`
   white-space: nowrap;
-  display: flex; /* Add this line to make the items flex containers */
-  align-items: center; /* Center items vertically within each NavItem */
 `;
 
 const NavLinkItem = styled(Link)`
@@ -90,87 +86,138 @@ const NavLinkItem = styled(Link)`
   text-decoration: none;
   display: flex;
   align-items: center;
-  gap: 5px; /* Decreased gap to align icons horizontally with text */
+  gap: 10px;
   transition: all 0.3s ease;
   padding: 10px;
   border-radius: 10px;
 
   &:hover {
     svg {
-      color: #67EB00; /* Change the color of SVG icons on hover */
-    }
-
-    img {
-      transform: scale(1.1); /* Scale the image on hover for a zoom effect */
-      box-shadow: 0 0 20px rgba(0, 0, 0, 0.4); /* Stronger box shadow on hover */
+      color: #67EB00;
     }
   }
 
   svg {
     font-size: 20px;
-    transition: all 0.3s ease; /* Add transition for smooth color change */
-    margin-right: 5px; /* Add right margin to separate the icon from text */
+    transition: all 0.3s ease;
+    margin-right: 5px;
   }
 
   img {
-    width: 40px; /* Increased width for a larger profile image */
-    height: 40px; /* Increased height for a larger profile image */
-    border: 2px solid #fff; /* White border around the image */
+    width: 40px;
+    height: 40px;
+    border: 2px solid #fff;
     border-radius: 50%;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Subtle box shadow effect */
-    transition: all 0.3s ease; /* Add smooth transition effect */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    transition: all 0.3s ease;
   }
 
-  /* Provide meaningful labels for accessibility */
   &[aria-label='Home']::before {
     content: 'Home';
-    display: none; /* Hide the label visually */
+    display: none;
   }
 
   &[aria-label='Profile']::before {
     content: 'Profile';
-    display: none; /* Hide the label visually */
+    display: none;
   }
 
   &[aria-label='Sign Up']::before {
     content: 'Sign Up';
-    display: none; /* Hide the label visually */
+    display: none;
   }
 
   &[aria-label='Blogs']::before {
     content: 'Blogs';
-    display: none; /* Hide the label visually */
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    justify-content: flex-start;
+    padding: 8px 16px;
+    box-sizing: border-box;
   }
 `;
 
 const HamburgerIcon = styled.div`
   display: none;
   cursor: pointer;
-  font-size: 24px;
+  font-size: 20px;
+  color: #fff;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: rotate(360deg) scale(1.2);
+  }
 
   @media (max-width: 768px) {
-    display: block;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 40px;
+    width: 40px;
+
+    div {
+      height: 6px;
+      width: 40px;
+      background-color: #fff;
+      border-radius: 8px;
+      transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+      margin: 6px 0;
+    }
+
+    &.open div:first-child {
+      transform: translateY(6px) rotate(-45deg);
+    }
+
+    &.open div:nth-child(2) {
+      transform: scaleX(0);
+    }
+
+    &.open div:last-child {
+      transform: translateY(-6px) rotate(45deg);
+    }
   }
 `;
+
 const HamburgerMenu = styled.div`
   @media (max-width: 768px) {
-    display: none; /* Hide the menu by default */
+    display: none;
 
-    /* If isOpen is true, display the menu as a column */
-    ${({ isOpen }) => isOpen && `
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      position: absolute;
-      top: 100%;
-      left: 0;
-      width: 100%;
-      background: #333;
-      padding: 10px;
-      transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
-      transform: translateY(0);
+    ${({ isOpen }) =>
+      isOpen &&
+      `
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        background: linear-gradient(to right, #5d4157, #a8caba);
+        padding: 20px;
+        transform-origin: top;
+        animation: rollAndFold 0.5s ease-in-out;
+        opacity: 1;
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+      `}
+  }
+
+  @keyframes rollAndFold {
+    0% {
+      transform: scaleY(0.1) translateY(-20px) rotate(0deg);
+      opacity: 0;
+    }
+    50% {
+      transform: scaleY(0.5) translateY(0) rotate(180deg);
       opacity: 1;
-    `}
+    }
+    100% {
+      transform: scaleY(1) translateY(0) rotate(360deg);
+      opacity: 1;
+    }
   }
 `;
 
@@ -185,6 +232,7 @@ function Header() {
     "Awaken Insight",
     "Brainwave Ballet"
   ];
+
   const [profileImage, setProfileImage] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -201,25 +249,33 @@ function Header() {
           Authorization: `Bearer ${token}`,
         },
       })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Error fetching user profile');
-      })
-      .then(data => {
-        if (data.profileImage) {
-          setProfileImage(`https://edu-back-j3mz.onrender.com/${data.profileImage}?key=${Date.now()}`);
-        } else {
-          setProfileImage('https://sanjaybasket.s3.ap-south-1.amazonaws.com/image.webp');
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching user profile:', error);
-        setProfileImage('https://sanjaybasket.s3.ap-south-1.amazonaws.com/image.webp');
-      });
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Error fetching user profile');
+        })
+        .then(data => {
+          if (data.profileImage) {
+            setProfileImage(
+              `https://edu-back-j3mz.onrender.com/${data.profileImage}?key=${Date.now()}`
+            );
+          } else {
+            setProfileImage(
+              'https://sanjaybasket.s3.ap-south-1.amazonaws.com/image.webp'
+            );
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching user profile:', error);
+          setProfileImage(
+            'https://sanjaybasket.s3.ap-south-1.amazonaws.com/image.webp'
+          );
+        });
     } else {
-      setProfileImage('https://sanjaybasket.s3.ap-south-1.amazonaws.com/image.webp');
+      setProfileImage(
+        'https://sanjaybasket.s3.ap-south-1.amazonaws.com/image.webp'
+      );
     }
   }, []);
 
@@ -231,21 +287,19 @@ function Header() {
             <EduxcelText className="eduxcel-text">Eduxcel</EduxcelText>
           </Logo>
         </Link>
-        <Typed
-          strings={typedTexts}
-          typeSpeed={40}
-          backSpeed={40}
-          loop
-        />
+        <Typed strings={typedTexts} typeSpeed={40} backSpeed={40} loop />
       </HeaderContent>
-      <HamburgerIcon onClick={toggleMenu}>
-        ☰
+      <HamburgerIcon className={isOpen ? 'open' : ''} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
       </HamburgerIcon>
       <HamburgerMenu isOpen={isOpen}>
         <NavList>
           <NavItem>
             <NavLinkItem to="/" aria-label="Home" onClick={toggleMenu}>
-              <FontAwesomeIcon icon={faHome} /> 
+              <FontAwesomeIcon icon={faHome} />
+              Home
             </NavLinkItem>
           </NavItem>
           <NavItem>
@@ -255,16 +309,19 @@ function Header() {
               ) : (
                 'Profile'
               )}
+              Profile
             </NavLinkItem>
           </NavItem>
           <NavItem>
             <NavLinkItem to="/signup" aria-label="Sign Up" onClick={toggleMenu}>
-              <FontAwesomeIcon icon={faUserPlus} /> 
+              <FontAwesomeIcon icon={faUserPlus} />
+              Sign Up
             </NavLinkItem>
           </NavItem>
           <NavItem>
             <NavLinkItem to="/blogs" aria-label="Blogs" onClick={toggleMenu}>
-              <FontAwesomeIcon icon={faNewspaper} /> 
+              <FontAwesomeIcon icon={faNewspaper} />
+              Blogs
             </NavLinkItem>
           </NavItem>
           {/* Add more navigation links */}
