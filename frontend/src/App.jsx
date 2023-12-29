7f79a9d510c12f1071b237f49b7acd64aeba37d0
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2023-12-29 21:10:39 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
-import  { useEffect } from "react";
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route  , useLocation} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
- import SubHeader from './components/SubHeader';
+import SubHeader from './components/SubHeader';
 import Home from './pages/Home';
 import UserProfile from './components/UserProfile';
 import ModuleDetails from './components/ModuleDetails';
@@ -10,27 +9,50 @@ import CourseDetails from './components/CourseDetails';
 import SubModuleDetails from './components/SubModuleDetails';
 import Blogs from './components/Blogs';
 import Footer from './components/Footer';
-import SignInSignUp from './components/SignInSignUp'; // Import the SignInSignUp component
+import SignInSignUp from './components/SignInSignUp';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
+
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    window.scrollTo(0, 0); // Scroll to the top of the page when the route changes.
-  }, [pathname]);
+    const unlisten = navigate(() => {
+      window.scrollTo(0, 0);
+    });
 
-  return null; // This component doesn't render anything.
+    return () => {}; // Use an empty function as the cleanup
+  }, [navigate]);
+
+  return null;
 };
 
 function App() {
+  const [isBlogsRoute, setIsBlogsRoute] = useState(false);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIsBlogsRoute(window.location.pathname.startsWith('/blogs'));
+    };
+
+    // Attach the event listener
+    window.addEventListener('popstate', handleRouteChange);
+
+    // Call it once to set the initial state
+    handleRouteChange();
+
+    // Detach the event listener on component unmount
+    return () => {
+      window.removeEventListener('popstate', handleRouteChange);
+    };
+  }, []);
   return (
     <Router>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Header />
     <SubHeader/>
-          <ScrollToTop />
+          
        
           <Routes>
             <Route path="/" element={<Home />} />
@@ -45,11 +67,11 @@ function App() {
             <Route path="/signin" element={<SignInSignUp />} /> {/* Use SignInSignUp component for sign-in */}
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset" element={<ResetPassword />} />
-               <Route path="/blogs/*" element={<Blogs />} />
+            <Route path="/blogs/*" element={<Blogs />} />
           </Routes>
         </div>
         <div className='relative z-0'>
-          <Footer />
+        {!isBlogsRoute && <Footer />}
         </div>
       </div>
     </Router>
