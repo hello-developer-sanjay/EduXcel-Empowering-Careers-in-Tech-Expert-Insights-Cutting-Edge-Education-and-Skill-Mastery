0db3376bb3f0774e853af89ad8d89ba9cc1f8c61
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-01-02 23:38:04 +0530

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import '../styles/AuthForms.css';
+import { css } from '@emotion/react';
+import { RingLoader } from 'react-spinners';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
@@ -15,6 +16,7 @@ function Signin() {
   const [userProfile, setUserProfile] = useState(null);
   const [signinError, setSigninError] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -23,7 +25,6 @@ function Signin() {
       (position) => {
         const { latitude, longitude } = position.coords;
         console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-        // Include latitude and longitude in the form data
         setFormData((prevData) => ({
           ...prevData,
           latitude,
@@ -36,11 +37,10 @@ function Signin() {
       { enableHighAccuracy: true }
     );
 
-    // To stop watching the position when the component unmounts:
     return () => {
       navigator.geolocation.clearWatch(watchId);
     };
-  }, []); // Run this effect only once on component mount
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -54,139 +54,45 @@ function Signin() {
     setShowPassword(!showPassword);
   };
 
-  const handleGoogleAuth = async () => {
-    try {
-      const token = localStorage.getItem('token');
-
-      if (token) {
-        try {
-          // Fetch the user profile data using the token
-          console.log('Fetching user profile...');
-          const profileResponse = await axios.get(
-            'https://edu-back-j3mz.onrender.com/api/profile',
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-
-          const userProfileData = profileResponse.data;
-          console.log('Received user profile data:', userProfileData);
-
-          // Set the user profile data in your state here
-          setUserProfile(userProfileData); // Set the state with fetched user profile data
-
-          // Navigate to the profile page
-          navigate('/profile');
-        } catch (error) {
-          console.error('Error fetching user profile:', error);
-        }
-      } else {
-        // Token is not present, initiate Google sign-in
-        const googleAuthUrl = 'https://edu-back-j3mz.onrender.com/auth/google';
-
-        const popup = window.open(
-          googleAuthUrl,
-          '_blank',
-          'width=600,height=600'
-        );
-
-        // Listen for messages from the popup window
-        window.addEventListener('message', async (event) => {
-          console.log('Received message from popup:', event);
-
-          if (
-            event.origin === 'https://edu-back-j3mz.onrender.com' &&
-            event.data.token
-          ) {
-            // Token received from the popup window
-            const newToken = event.data.token;
-            console.log('Received token:', newToken);
-
-            // Store the token in local storage
-            localStorage.setItem('token', newToken);
-
-            try {
-              // Fetch the user profile data using the token
-              console.log('Fetching user profile...');
-              const profileResponse = await axios.get(
-                'https://edu-back-j3mz.onrender.com/api/profile',
-                {
-                  headers: {
-                    Authorization: `Bearer ${newToken}`,
-                  },
-                }
-              );
-
-              const userProfileData = profileResponse.data;
-              console.log('Received user profile data:', userProfileData);
-
-              // Set the user profile data in your state here
-              setUserProfile(userProfileData); // Set the state with fetched user profile data
-
-              // Close the popup window
-              popup.close();
-
-              // Redirect to the profile page
-              navigate('/profile');
-            } catch (error) {
-              console.error('Error fetching user profile:', error);
-            }
-          }
-        });
-      }
-    } catch (error) {
-      console.error('Google authentication error:', error);
-    }
-  };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+
     try {
-      const response = await axios.post(
-        'https://edu-back-j3mz.onrender.com/api/signin',
-        formData
-      );
+      const response = await axios.post('https://edu-back-j3mz.onrender.com/api/signin', formData);
       console.log('Signin success');
       const token = response.data.token;
 
-      // Store the token in local storage
       localStorage.setItem('token', token);
 
-      // Fetch the user profile data here
-      const profileResponse = await axios.get(
-        'https://edu-back-j3mz.onrender.com/api/profile',
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const profileResponse = await axios.get('https://edu-back-j3mz.onrender.com/api/profile', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
       const userProfileData = profileResponse.data;
-
-      // Set the user profile data in your state here
-      setUserProfile(userProfileData); // Add this line to set the user profile in the state
-
+      setUserProfile(userProfileData);
       navigate('/profile');
     } catch (error) {
       console.error('Signin error:', error.response.data.message);
       setSigninError(error.response.data.message);
+    } finally {
+      setLoading(false);
     }
   };
+
+  const override = css`
+    display: block;
+    margin: 0 auto;
+  `;
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={handleChange}
-          />
+          <input type="email" name="email" placeholder="Email" onChange={handleChange} />
         </div>
         <div className="form-group">
           <label>Password:</label>
@@ -205,15 +111,17 @@ function Signin() {
           </div>
         </div>
         <button type="submit" className="form-button">
-          Sign In
+          {loading ? (
+            <RingLoader color={'#ffffff'} loading={loading} css={override} size={30} />
+          ) : (
+            'Sign In'
+          )}
         </button>
         <Link to="/forgot-password" className="forgot-password-link">
           Forgot Password
         </Link>
       </form>
       {signinError && <p className="error-message">{signinError}</p>}
-
-      {/* Google Authentication Button */}
     </div>
   );
 }
