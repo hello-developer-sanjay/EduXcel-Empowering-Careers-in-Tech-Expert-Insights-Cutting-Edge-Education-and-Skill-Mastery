e99fd9525fd33cdc17f91a4d2b102599f2e8a8f9
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-02-18 22:35:37 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -430,7 +430,7 @@ let endpoint = "submit-feedback";
         successMessage = "Query sent! Await our swift reply, tailored just for you.";
       }
 
-      const response = await fetch(`https://edu-back-j3mz.onrender.com/api/${endpoint}`, {
+      const response = await fetch(`https://edu-j3mz.onrender.com/api/${endpoint}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
