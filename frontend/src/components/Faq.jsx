7615c3291bef5bb1d3a24918d0e9c7ee360de220
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-03-10 21:31:43 +0530

    Update Faq.jsx

--- frontend/src/components/Faq.jsx
+++ frontend/src/components/Faq.jsx
@@ -4,19 +4,27 @@ import '../styles/Faq.css';
 const Faq = () => {
   const faqs = [
     {
-      question: 'What courses do you offer?',
-      answer: 'We offer a wide range of courses including Mathematics, Science, History, Programming, and many more. Visit our Courses page for more details.',
+      question: 'How can EduXcel help me advance my career in technology?',
+      answer: 'Discover how EduXcels expert-led courses and cutting-edge curriculum can propel your career forward in the dynamic field of technology. Gain practical skills, stay updated with industry trends, and unlock new opportunities with EduXcel.',
     },
     {
-      question: 'How can I enroll in a course?',
-      answer: 'To enroll in a course, you need to create an account on our website, choose the desired course, and follow the enrollment process provided on the course page.',
+      question: 'What sets EduXcel apart from other online learning platforms?',
+      answer: 'Learn what makes EduXcel stand out in the crowded online learning landscape. From personalized learning paths to industry-recognized certifications, explore how EduXcel is redefining the future of tech education for aspiring professionals worldwide.',
     },
     {
-      question: 'Is there any free trial available?',
-      answer: 'Yes, we offer free trials for some of our courses. You can check the course details to see if a free trial is available.',
+      question: 'Are EduXcel courses suitable for beginners?',
+      answer: "Find out if EduXcel's courses are suitable for beginners looking to kickstart their journey in technology. Whether you're new to coding or seeking to enhance your technical skills, EduXcel offers beginner-friendly courses designed to support learners at every level.",
     },
-    
+    {
+      question: 'Can I access EduXcel courses on mobile devices?',
+      answer: 'Discover the flexibility of learning with EduXcel anytime, anywhere. Explore how our mobile-friendly platform allows you to access course materials, participate in interactive sessions, and track your progress seamlessly on your smartphone or tablet.',
+    },
+    {
+      question: 'Does EduXcel offer scholarships or financial aid?',
+      answer: "Learn about EduXcel's commitment to making quality education accessible to all. Explore our scholarship and financial aid options, and discover how you can pursue your educational goals with support from EduXcel.",
+    },
   ];
+  
 
   const [activeIndex, setActiveIndex] = useState(null);
 
