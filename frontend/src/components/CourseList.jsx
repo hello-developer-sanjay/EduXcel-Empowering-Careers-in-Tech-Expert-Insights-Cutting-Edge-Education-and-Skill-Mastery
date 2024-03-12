81505fdb46074c9920ed58a5328e4d9893003d74
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-03-12 16:24:26 +0530

    Update CourseList.jsx

--- frontend/src/components/CourseList.jsx
+++ frontend/src/components/CourseList.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/CourseList.css';
 import courseImage1 from '../assets/html.png';
 import courseImage2 from '../assets/css.png';
 import courseImage3 from '../assets/responsive1.png';
 import courseImage4 from '../assets/preprocessors.png';
-
 import SignInSignUp from './SignInSignUp';
 
 function CourseList() {
   const [courses, setCourses] = useState([]);
   const [showSignInSignUp, setShowSignInSignUp] = useState(false);
+  const [redirectCategory, setRedirectCategory] = useState(null);
   const { category } = useParams();
 
   useEffect(() => {
@@ -38,44 +39,60 @@ function CourseList() {
 
   const uniqueCategories = Array.from(new Set(courses.map(course => course.category)));
 
-  // Function to handle hover over course card
-  const handleCourseCardHover = () => {
-    // Only show the signup form if the user is not signed in
-    if (!localStorage.getItem('token')) {
+  const handleCourseCardClick = (clickedCategory) => {
+    if (localStorage.getItem('token')) {
+      setRedirectCategory(clickedCategory);
+    } else {
       setShowSignInSignUp(true);
+      toast.info('Please sign in to view the course details', {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
   };
 
-  // Function to handle hover out of course card
-  const handleCourseCardHoverOut = () => {
-    // Prevent closing when hovering over the form
-    if (!showSignInSignUp) {
-      setShowSignInSignUp(false);
-    }
-  };
-
-  // Function to close the form
   const handleCloseForm = () => {
     setShowSignInSignUp(false);
   };
 
-  return (
-  
-    <section className={`relative w-full min-h-screen mx-auto`}>
+  const getImageForCategory = (category) => {
+    switch (category) {
+      case 'html_courses':
+        return courseImage1;
+      case 'css_courses':
+        return courseImage2;
+      case 'responsive_web_design_courses':
+        return courseImage3;
+      case 'css_preprocessors_courses':
+        return courseImage4;
+      default:
+        return '';
+    }
+  };
 
-    <div className="course-list">
-      <div className="course-cards">
-        {uniqueCategories.map((category, index) => {
-          const course = courses.find(course => course.category === category);
-          const image = getImageForCategory(category);
-          return (
-            <div
-              className="course-card"
-              key={index} // Use index as key since category itself is not unique
-              onMouseEnter={handleCourseCardHover}
-              onMouseLeave={handleCourseCardHoverOut}
-            >
-              <Link to={`/course/${category}`}>
+  if (redirectCategory) {
+    window.location.href = `/course/${redirectCategory}`;
+    return null;
+  }
+
+  return (
+    <section className={`relative w-full min-h-screen mx-auto`}>
+      <div className="course-list">
+        <div className="course-cards">
+          {uniqueCategories.map((category, index) => {
+            const course = courses.find(course => course.category === category);
+            const image = getImageForCategory(category);
+            return (
+              <div
+                className="course-card"
+                key={index}
+                onClick={() => handleCourseCardClick(category)}
+              >
                 <div
                   className="course-image"
                   style={{
@@ -86,40 +103,22 @@ function CourseList() {
                   <h3>{category}</h3>
                   <p>{course.description}</p>
                 </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-      {showSignInSignUp && (
-        <div className="sign-in-sign-up-container">
-          <button className="close-button" onClick={handleCloseForm}>
-            Close
-          </button>
-          <SignInSignUp />
+              </div>
+            );
+          })}
         </div>
-      )}
-    </div>
-  
+        {showSignInSignUp && (
+          <div className="sign-in-sign-up-container">
+            <button className="close-button" onClick={handleCloseForm}>
+              Close
+            </button>
+            <SignInSignUp />
+          </div>
+        )}
+      </div>
+      <ToastContainer />
     </section>
-
   );
-}
-
-// Function to get the image URL for each category
-function getImageForCategory(category) {
-  switch (category) {
-    case 'html_courses':
-      return courseImage1;
-    case 'css_courses':
-      return courseImage2;
-    case 'responsive_web_design_courses':
-      return courseImage3;
-      case 'css_preprocessors_courses':
-      return courseImage4;
-    default:
-      return ''; 
-  }
 }
 
 export default CourseList;
