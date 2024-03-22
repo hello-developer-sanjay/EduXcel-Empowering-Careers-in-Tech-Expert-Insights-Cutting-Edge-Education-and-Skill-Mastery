4c266204de46bce284013c74c9b5c415c02df6dd
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-03-22 23:43:53 +0530

    Update ResetPassword.jsx

--- frontend/src/components/ResetPassword.jsx
+++ frontend/src/components/ResetPassword.jsx
@@ -3,7 +3,8 @@ import  { useState } from 'react';
 import '../styles/ResetPassword.css'; // Import the CSS file for styling
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-
+import { Helmet } from 'react-helmet';
+  
 function ResetPassword() {
   const [newPassword, setNewPassword] = useState('');
   const [token, setToken] = useState('');
@@ -38,6 +39,223 @@ function ResetPassword() {
 
   return (
     <div className="reset-password-container">
+         <Helmet>
+    
+    <title>Reset Your Password | EduXcel: Account Recovery</title>
+ <meta
+   name="description"
+   content="User data is protected with state-of-the-art security measures. Explore our courses and enhance your skills with Eduxcel. Find a wide range of online courses on various topics to boost your knowledge. Discover the remarkable journey of Sanjay Patidar, a dedicated and passionate individual excelling in the realms of web development and UI/UX design. As a Computer Science and Engineering student, Sanjay embodies a profound commitment to mastering the intricacies of programming, with a specialized focus on crafting immersive digital experiences. Currently pursuing a Bachelor of Engineering degree at Chandigarh University, Sanjay brings forth a proactive approach and an unwavering dedication to excellence in all endeavors. His educational odyssey began at Jawahar Navodaya Vidyalaya School, where he laid the foundation for his academic and extracurricular prowess from 2009 to 2016. This esteemed residential institution, administered under the Ministry of Education, fostered holistic development and instilled a fervent pursuit of excellence. Throughout his tenure, Sanjay actively engaged in a plethora of inter-school competitions, showcasing his versatility and achieving distinction in both academic and non-academic domains. Building upon this robust foundation, Sanjay embarked on his tertiary education journey at Chandigarh University, where he embarked on a quest for knowledge and innovation. Embracing the cutting-edge infrastructure and progressive pedagogical approaches of the university, he delved deep into the intricacies of Computer Science Engineering. His academic trajectory is marked by a stellar GPA of 7.5, a testament to his unwavering dedication and intellectual acumen. Beyond the confines of academia, Sanjay's passion for technology extends to the realms of practical application. Actively participating in coding competitions, hackathons, and collaborative projects, he honed his problem-solving prowess and cultivated invaluable teamwork skills. His journey is characterized by a relentless pursuit of excellence, driven by a profound sense of creativity, innovation, and a commitment to delivering exceptional digital solutions. Explore the professional portfolio of Sanjay Patidar, where innovation meets expertise, and witness firsthand the culmination of passion, creativity, and a relentless pursuit of excellence in web development and UI/UX design."
+ />
+ 
+
+ <meta property="og:title" content=" Reset Your Password | EduXcel: Account Recovery" />
+ <meta property="og:description" content="User data is protected with state-of-the-art security measures.Explore our courses and enhance your skills with Eduxcel. Find a wide range of online courses on various topics to boost your knowledge. Discover the remarkable journey of Sanjay Patidar, a dedicated and passionate individual excelling in the realms of web development and UI/UX design. As a Computer Science and Engineering student, Sanjay embodies a profound commitment to mastering the intricacies of programming, with a specialized focus on crafting immersive digital experiences. Currently pursuing a Bachelor of Engineering degree at Chandigarh University, Sanjay brings forth a proactive approach and an unwavering dedication to excellence in all endeavors. His educational odyssey began at Jawahar Navodaya Vidyalaya School, where he laid the foundation for his academic and extracurricular prowess from 2009 to 2016. This esteemed residential institution, administered under the Ministry of Education, fostered holistic development and instilled a fervent pursuit of excellence. Throughout his tenure, Sanjay actively engaged in a plethora of inter-school competitions, showcasing his versatility and achieving distinction in both academic and non-academic domains. Building upon this robust foundation, Sanjay embarked on his tertiary education journey at Chandigarh University, where he embarked on a quest for knowledge and innovation. Embracing the cutting-edge infrastructure and progressive pedagogical approaches of the university, he delved deep into the intricacies of Computer Science Engineering. His academic trajectory is marked by a stellar GPA of 7.5, a testament to his unwavering dedication and intellectual acumen. Beyond the confines of academia, Sanjay's passion for technology extends to the realms of practical application. Actively participating in coding competitions, hackathons, and collaborative projects, he honed his problem-solving prowess and cultivated invaluable teamwork skills. His journey is characterized by a relentless pursuit of excellence, driven by a profound sense of creativity, innovation, and a commitment to delivering exceptional digital solutions. Explore the professional portfolio of Sanjay Patidar, where innovation meets expertise, and witness firsthand the culmination of passion, creativity, and a relentless pursuit of excellence in web development and UI/UX design." />
+ <meta property="og:type" content="website" />
+ <meta property="og:url" content="https://eduxcel.vercel.app/reset" />
+ <meta property="og:image" content="https://sanjaybasket.s3.ap-south-1.amazonaws.com/logo_eduxcel.jpg" />
+ <meta property="og:image:alt" content="Sanjay Patidar" />
+ <meta property="og:site_name" content="Reset Your Password | EduXcel: Account Recovery" />
+  <link rel="canonical" href="https://eduxcel.vercel.app/reset" />
+
+ <meta name="twitter:card" content="summary_large_image" />
+ <meta name="twitter:title" content="Reset Your Password | EduXcel: Account Recovery" />
+ <meta name="twitter:description" content="User data is protected with state-of-the-art security measures. Explore our courses and enhance your skills with Eduxcel. Find a wide range of online courses on various topics to boost your knowledge. Discover the remarkable journey of Sanjay Patidar, a dedicated and passionate individual excelling in the realms of web development and UI/UX design. As a Computer Science and Engineering student, Sanjay embodies a profound commitment to mastering the intricacies of programming, with a specialized focus on crafting immersive digital experiences. Currently pursuing a Bachelor of Engineering degree at Chandigarh University, Sanjay brings forth a proactive approach and an unwavering dedication to excellence in all endeavors. His educational odyssey began at Jawahar Navodaya Vidyalaya School, where he laid the foundation for his academic and extracurricular prowess from 2009 to 2016. This esteemed residential institution, administered under the Ministry of Education, fostered holistic development and instilled a fervent pursuit of excellence. Throughout his tenure, Sanjay actively engaged in a plethora of inter-school competitions, showcasing his versatility and achieving distinction in both academic and non-academic domains. Building upon this robust foundation, Sanjay embarked on his tertiary education journey at Chandigarh University, where he embarked on a quest for knowledge and innovation. Embracing the cutting-edge infrastructure and progressive pedagogical approaches of the university, he delved deep into the intricacies of Computer Science Engineering. His academic trajectory is marked by a stellar GPA of 7.5, a testament to his unwavering dedication and intellectual acumen. Beyond the confines of academia, Sanjay's passion for technology extends to the realms of practical application. Actively participating in coding competitions, hackathons, and collaborative projects, he honed his problem-solving prowess and cultivated invaluable teamwork skills. His journey is characterized by a relentless pursuit of excellence, driven by a profound sense of creativity, innovation, and a commitment to delivering exceptional digital solutions. Explore the professional portfolio of Sanjay Patidar, where innovation meets expertise, and witness firsthand the culmination of passion, creativity, and a relentless pursuit of excellence in web development and UI/UX design." />
+ <meta name="twitter:image" content="https://sanjaybasket.s3.ap-south-1.amazonaws.com/logo_eduxcel.jpg" />
+ <meta name="twitter:site" content="@sanjaypatidar" />
+ <meta name="twitter:creator" content="@sanjaypatidar" />
+
+ <meta name="keywords" content="portfolio, signup , secure, eduxcel ,founder: Sanjay patidar , tech, education, careers, opportunity, personal-portfolio,developer_sanju,sanjay, Sanjay, SANJAY, Sanjay Patidar, SANJAY PATIDAR, SANJAY WEB DEVELOPER, SANJAY DEVELOPER, Full Stack Web Developer, Mern Stack Web Developer, sanjay patidar, sanjay-patidar, professional, web developer portfolio, coder, web development, UI/UX design, Chandigarh University, EduXcel, Indore,contact, developer, programmer, engineer, AI, Artificial Intelligence ,tech enthusiastic, creativity ,creator, work , technology, coding, projects, experiences, resume, cv" />
+ <meta name="author" content="EduXcel" />        <script type="application/ld+json">
+       {JSON.stringify({
+         '@context': 'http://schema.org',
+         '@type': 'ItemList',
+         "name": "Sanjay Patidar",
+         "birthDate": "1998-07-01",
+         "birthPlace": {
+           "@type": "Place",
+           "address": {
+             "@type": "PostalAddress",
+             "addressLocality": "Indore"
+           }
+         },
+         "alumniOf": {
+           "@type": "CollegeOrUniversity",
+           "name": "Chandigarh University",
+           "location": {
+             "@type": "Place",
+             "address": {
+               "@type": "PostalAddress",
+               "addressLocality": "Chandigarh",
+               "addressRegion": "Punjab",
+               "addressCountry": "India"
+             }
+           }
+         },
+         "address": [
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Indore",
+             "addressRegion": "Madhya Pradesh",
+             "postalCode": "452001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Chandigarh",
+             "addressRegion": "Punjab",
+             "postalCode": "160001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Mumbai",
+             "addressRegion": "Maharashtra",
+             "postalCode": "400001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Bangalore",
+             "addressRegion": "Karnataka",
+             "postalCode": "560001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Delhi",
+             "addressRegion": "Delhi",
+             "postalCode": "110001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Kolkata",
+             "addressRegion": "West Bengal",
+             "postalCode": "700001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Chennai",
+             "addressRegion": "Tamil Nadu",
+             "postalCode": "600001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Hyderabad",
+             "addressRegion": "Telangana",
+             "postalCode": "500001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Pune",
+             "addressRegion": "Maharashtra",
+             "postalCode": "411001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Ahmedabad",
+             "addressRegion": "Gujarat",
+             "postalCode": "380001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Jaipur",
+             "addressRegion": "Rajasthan",
+             "postalCode": "302001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Lucknow",
+             "addressRegion": "Uttar Pradesh",
+             "postalCode": "226001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Bhopal",
+             "addressRegion": "Madhya Pradesh",
+             "postalCode": "462001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Nagpur",
+             "addressRegion": "Maharashtra",
+             "postalCode": "440001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Visakhapatnam",
+             "addressRegion": "Andhra Pradesh",
+             "postalCode": "530001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Kochi",
+             "addressRegion": "Kerala",
+             "postalCode": "682001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Guwahati",
+             "addressRegion": "Assam",
+             "postalCode": "781001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Bhubaneswar",
+             "addressRegion": "Odisha",
+             "postalCode": "751001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Dehradun",
+             "addressRegion": "Uttarakhand",
+             "postalCode": "248001",
+             "addressCountry": "India"
+           },
+           {
+             "@type": "PostalAddress",
+             "addressLocality": "Raipur",
+             "addressRegion": "Chhattisgarh",
+             "postalCode": "492001",
+             "addressCountry": "India"
+           }
+         ],
+         "worksFor": {
+           "@type": "Organization",
+           "name": "Eduxcel" 
+         },
+         "url": "https://sanjay-patidar.vercel.app/",
+         "sameAs": [
+           "https://www.linkedin.com/in/sanjay-patidar-25b580292/",
+           "https://github.com/hello-developer-sanjay",
+           "https://www.instagram.com/sanjay_patidar_mcmxcviii/",
+           "https://eduxcel.vercel.app/",
+           "https://eduxcel.vercel.app/forgot-password",
+
+                        "https://eduxcel.vercel.app/signup"
+
+         ]
+   
+
+       })}
+     </script>
+
+
+    </Helmet>
+     
         <h1 className="reset-heading">Password Reset | Eduxcel</h1>
       {message && <p className={`message ${message === 'Password reset successfully' ? 'success' : 'error'}`}>{message}</p>}
       <form onSubmit={handleSubmit}>
