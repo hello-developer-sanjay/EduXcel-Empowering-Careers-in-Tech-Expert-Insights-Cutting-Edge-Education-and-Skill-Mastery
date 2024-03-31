4ee93d66e7c371abf5d900cb1631e898fcd57b14
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-03-31 15:16:30 +0530

    Update CourseDetailes.jsx

--- frontend/src/components/CourseDetailes.jsx
+++ frontend/src/components/CourseDetailes.jsx
@@ -547,7 +547,6 @@ if (matchSpecialChars) {
       return (
 <>  
        <Helmet>
-        <title>{`${currentPosts.length > 0 ? currentPosts[0].title : ""} | EduXcel | Sanjay Patidar`}</title>
                         <link rel="canonical" href={window.location.href} />
 
       </Helmet>
