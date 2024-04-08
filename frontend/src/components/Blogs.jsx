497e9d95781fd90ab14f40e32fdcb525d15a6b7f
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-08 12:14:38 +0530

    Update Blogs.jsx

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -162,7 +162,7 @@ import 'video.js/dist/video-js.css';
       const fetchData = async (collection) => {
         try {
           const response = await fetch(
-            `https://portfolio-api-b53a.onrender.com/api/${collection}`
+            `https://portfolio-api2-zhoj.onrender.com/api/${collection}`
           );
           const responseData = await response.json();
           setBlogsData((prevData) => ({
