8498a31f3d8ae870609017264e29098723032406
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-08 16:13:22 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -179,7 +179,7 @@ function Home() {
   useEffect(() => {
     async function fetchCourses() {
       try {
-        const response = await axios.get('https://eduxcel-api3.onrender.com/api/courses');
+        const response = await axios.get('https://eduxcel-api3-j9a2.onrender.com/api/courses');
         setCourseData(response.data);
       } catch (error) {
         console.error('Error fetching courses:', error);
