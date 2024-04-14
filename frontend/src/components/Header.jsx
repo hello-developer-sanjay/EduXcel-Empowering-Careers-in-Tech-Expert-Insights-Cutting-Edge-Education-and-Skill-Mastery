55bbc92bf7bba93b758d8f9e65f4544b4af14aac
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-14 11:50:30 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -305,7 +305,7 @@ function Header() {
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      fetch('https://eduxcel-api-13april.onrender.com/api/profile', {
+      fetch('https://eduxcel-api-14april.onrender.com/api/profile', {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`,
@@ -320,7 +320,7 @@ function Header() {
         .then(data => {
           if (data.profileImage) {
             setProfileImage(
-              `https://eduxcel-api-13april.onrender.com/${data.profileImage}?key=${Date.now()}`
+              `https://eduxcel-api-14april.onrender.com/${data.profileImage}?key=${Date.now()}`
             );
           } else {
             setProfileImage(
