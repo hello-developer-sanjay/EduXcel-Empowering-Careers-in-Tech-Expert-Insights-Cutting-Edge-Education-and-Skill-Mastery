e7a930a133055e3a5b7ad146e464d50dbbbb7c7a
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-04-30 10:53:53 +0530

    Update UserProfile.jsx

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -59,7 +59,7 @@ useEffect(() => {
         return;
       }
 
-      const response = await fetch('https://eduxcel-api-14april.onrender.com/api/profile', {
+      const response = await fetch('https://eduxcel-api-30april.onrender.com/api/profile', {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`,
@@ -97,7 +97,7 @@ useEffect(() => {
  const handleUpdateProfile = async (updatedProfileData) => {
     try {
       const token = localStorage.getItem('token');
-      const response = await fetch('https://eduxcel-api-14april.onrender.com/api/profile', {
+      const response = await fetch('https://eduxcel-api-30april.onrender.com/api/profile', {
         method: 'PUT',
         headers: {
           Authorization: `Bearer ${token}`,
@@ -125,7 +125,7 @@ useEffect(() => {
   const confirmLogout = async () => {
     try {
       // Send a request to the server to log the user out
-      await axios.post('https://eduxcel-api-14april.onrender.com/api/logout');
+      await axios.post('https://eduxcel-api-30april.onrender.com/api/logout');
       // Clear the token from local storage
       localStorage.removeItem('token');
       // Redirect the user to the login page using navigate
@@ -176,7 +176,7 @@ useEffect(() => {
  <meta name="author" content="EduXcel" />        <script type="application/ld+json">
        {JSON.stringify({
          '@context': 'http://schema.org',
-         '@type': 'ItemList',
+         '@type': 'Person',
          "name": "Sanjay Patidar",
          "birthDate": "1998-07-01",
          "birthPlace": {
@@ -384,7 +384,7 @@ useEffect(() => {
   <div className="profile-info">
     <div className="profile-image-container">
       <motion.img
-        src={`https://eduxcel-api-14april.onrender.com/${userProfile.profileImage}?key=${Date.now()}`}
+        src={`https://eduxcel-api-30april.onrender.com/${userProfile.profileImage}?key=${Date.now()}`}
         alt="Profile"
         className="profile-image"
         whileHover={{ scale: 1.1 }}
