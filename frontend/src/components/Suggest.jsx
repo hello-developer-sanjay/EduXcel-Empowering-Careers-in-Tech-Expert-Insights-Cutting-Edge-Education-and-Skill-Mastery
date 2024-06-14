5d685338f450a33c24139435ef8a8a6ea26f9366
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-06-15 02:57:27 +0530

    Update Suggest.jsx

--- frontend/src/components/Suggest.jsx
+++ frontend/src/components/Suggest.jsx
@@ -142,7 +142,7 @@ const Suggest = () => {
   useEffect(() => {
     const fetchRandomBlogTitles = async () => {
       try {
-        const response = await axios.get('https://eduxcel-api-5jun.onrender.com/api/random-blog-titles');
+        const response = await axios.get('https://eduxcel-api-15jun.onrender.com/api/random-blog-titles');
         setRandomBlogTitles(response.data);
         setAnimationTrigger(true); // Trigger animation after data fetch
       } catch (error) {
