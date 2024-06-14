6abf9de7b0f26076ad8d4a3f0b8f7a75d157addb
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-06-15 02:24:12 +0530

    Update CareerBlog.jsx

--- frontend/src/components/CareerBlog.jsx
+++ frontend/src/components/CareerBlog.jsx
@@ -132,7 +132,7 @@ import { Helmet } from "react-helmet";
 
       const fetchDataForvision = async (vision) => {
         try {
-          const response = await fetch(`https://eduxcel-api-5jun.onrender.com/api/${vision}`);
+          const response = await fetch(`https://eduxcel-api-15jun.onrender.com/api/${vision}`);
           const responseData = await response.json();
           setBlogsData((prevData) => ({
             ...prevData,
