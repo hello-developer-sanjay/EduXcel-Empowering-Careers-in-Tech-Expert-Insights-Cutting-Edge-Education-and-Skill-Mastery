64a0a2e56994b171ed32fc408ec9fcbafa57f2ea
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-08-05 21:42:56 +0530

    Update CollegePost.jsx

--- frontend/src/components/CollegePost.jsx
+++ frontend/src/components/CollegePost.jsx
@@ -135,7 +135,7 @@ import { Helmet } from "react-helmet";
 
       const fetchDataForinstitute = async (institute) => {
         try {
-          const response = await fetch(`https://eduxcel-api-16july.onrender.com/api/${institute}`);
+          const response = await fetch(`https://eduxcel-api-5aug.onrender.com/api/${institute}`);
           const responseData = await response.json();
           setBlogsData((prevData) => ({
             ...prevData,
