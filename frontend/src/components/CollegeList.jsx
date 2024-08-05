a79dc931867ebb942e668fb528299c61e04500b6
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2024-08-05 21:35:42 +0530

    Update CollegeList.jsx

--- frontend/src/components/CollegeList.jsx
+++ frontend/src/components/CollegeList.jsx
@@ -28,12 +28,12 @@ function CollegeList() {
     try {
         let response;
         if (!institute || institute === 'all') {
-          response = await axios.get('https://eduxcel-api-16july.onrender.com/api/institute/all');
+          response = await axios.get('https://eduxcel-api-5aug.onrender.com/api/institute/all');
         } else {
-          response = await axios.get(`https://eduxcel-api-16july.onrender.com/api/institute/${institute}`);
+          response = await axios.get(`https://eduxcel-api-5aug.onrender.com/api/institute/${institute}`);
         }
         if (!response) {
-          response = await axios.get('https://eduxcel-api-16july.onrender.com/api/institute');
+          response = await axios.get('https://eduxcel-api-5aug.onrender.com/api/institute');
         }
         setJournals(response.data);
         setLoading(false); 
